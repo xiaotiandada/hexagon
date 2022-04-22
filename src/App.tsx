b668faf9da5a901bdc8eeb0f436db0ebcfcc8dce
_XiaoTian <istianlei@qq.com>
2022-04-23 02:02:55 +0800

    hexagon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 /* eslint-disable no-unused-vars */
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {useWasm} from 'react-wasm';
 import {Stage, Layer, Rect, Text, Line, RegularPolygon, Group} from 'react-konva';
 import Konva from 'konva';
 import {drawGrid} from './utils';
+import {geoToH3, kRing, polyfill, h3SetToMultiPolygon} from 'h3-js';
+import * as PIXI from 'pixi.js';
+import * as Honeycomb from 'honeycomb-grid';
 
 const ExampleComponent = (): any => {
 	const {
@@ -28,46 +31,46 @@ const ExampleComponent = (): any => {
 	return <div>1 + 2 = {instance.exports.add(1, 2)}</div>;
 };
 
-const cubeToAxial = (x: number, y: number, z: number): any => ({
-	x,
-	y: z,
-});
-
 function App() {
-	const [points, setPoints] = useState<any[]>([]);
+	const canvasContainerRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
-		setPoints(drawGrid(50, 50));
+		if (canvasContainerRef.current) {
+			const app = new PIXI.Application({transparent: true});
+			const graphics = new PIXI.Graphics();
+
+			const Hex = Honeycomb.extendHex({size: 50});
+			const Grid = Honeycomb.defineGrid(Hex);
+
+			canvasContainerRef.current.appendChild(app.view);
+			// Set a line style of 1px wide and color #999
+			graphics.lineStyle(1, 0x999999);
+
+			// Render 10,000 hexes
+			Grid.hexagon({
+				radius: 2,
+				center: [5, 5],
+			}).forEach(hex => {
+				const point = hex.toPoint();
+				// Add the hex's position to each of its corner points
+				const corners = hex.corners().map(corner => corner.add(point));
+				// Separate the first from the other corners
+				const [firstCorner, ...otherCorners] = corners;
+
+				// Move the "pen" to the first corner
+				graphics.moveTo(firstCorner.x, firstCorner.y);
+				// Draw lines to the other corners
+				otherCorners.forEach(({x, y}) => graphics.lineTo(x, y));
+				// Finish at the first corner
+				graphics.lineTo(firstCorner.x, firstCorner.y);
+
+				app.stage.addChild(graphics);
+			});
+		}
 	}, []);
 	return (
 		<>
-			<Stage width={window.innerWidth} height={window.innerHeight}>
-				<Layer>
-					<Group draggable={true}>
-						{
-							points.map((point, index) => {
-								const {x, y} = cubeToAxial(point.q, point.s, point.r);
-								return (
-									<Group
-										key={index}
-										x={x * 100}
-										y={y * 100}
-									>
-										<RegularPolygon
-											sides={6}
-											radius={50}
-											stroke={'#333'}
-											rotation={0}
-										>
-										</RegularPolygon>
-										<Text text={`${point.q},${point.s},${point.r}`}></Text>
-									</Group>
-								);
-							})
-						}
-					</Group>
-				</Layer>
-			</Stage>
+			<div ref={canvasContainerRef}></div>
 		</>
 	);
 }
